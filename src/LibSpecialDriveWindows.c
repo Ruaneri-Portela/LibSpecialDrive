5ca0d9946684d535ec2f5866ee80d52e98e8180c
Ruaneri Portela <ruaneriportela@outlook.com>
2025-05-21 12:35:36 -0300

    Add DLL templete (win)

--- src/LibSpecialDriveWindows.c
+++ src/LibSpecialDriveWindows.c
@@ -237,6 +237,37 @@ void LibSpecialDriveCloseDevice(LibSpecialDrive_DeviceHandle device)
         fprintf(stderr, "CloseHandle failed (Error: %lu)\n", GetLastError());
     }
 }
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason,LPVOID lpvReserved)
+{
+    // Perform actions based on the reason for calling.
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        // Initialize once for each new process.
+        // Return FALSE to fail DLL load.
+        break;
+
+    case DLL_THREAD_ATTACH:
+        // Do thread-specific initialization.
+        break;
+
+    case DLL_THREAD_DETACH:
+        // Do thread-specific cleanup.
+        break;
+
+    case DLL_PROCESS_DETACH:
+
+        if (lpvReserved != NULL)
+        {
+            break; // do not do cleanup if process termination scenario
+        }
+
+        // Perform any necessary cleanup.
+        break;
+    }
+    return TRUE;  // Successful DLL_PROCESS_ATTACH.
+}
 #else
 #define LIBSPECIALDRIVEMWIN_C_EMPTY
 void LibSpecialDriveWIN_dummy(void) {}
